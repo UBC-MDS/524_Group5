95599ab1c7f7cceca4026b14a777cd3e9210c36b
Robin Dhillon <robindhillon1@hotmail.com>
2023-01-17 21:47:35 -0800

    functions working but not final

--- src/stocksignals/move_ave_200days.py
+++ src/stocksignals/move_ave_200days.py
@@ -20,8 +20,9 @@ def move_ave_200days(stock_data):
     >>> move_ave_200days(MSFT_data)
     """
     data = pd.read_csv('data/'+stock_data+'.csv')
-    print(data)
+    data.index = pd.to_datetime(data["Date"], utc=True).dt.date
     mov_avg = data.rolling(window=200).mean()
+    mov_avg.index = data.index
     return mov_avg
 
-move_ave_200days("MSFT")
+print(move_ave_200days("MSFT"))
